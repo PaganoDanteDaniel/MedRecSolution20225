3bfbb4c5ac3900dbdbd878d50b091ddfe14db7a8
Pagano, Dante Daniel <dpagano@mendoza.edu.ar>
2026-08-06 11:55:22 -0300

    fix(identity): IAuthenticateUserOutputPort debe extender IBaseOutputPort
    ICommonOutputPort declara ValidationErrors/ErrorMessage que BaseOutputPort<T>
    no implementa; los 28 output ports restantes del código extienden
    IBaseOutputPort. Corrige la inconsistencia introducida en la Task 5 para que
    AuthenticateUserPresenter (Task 12) compile siguiendo la convención existente.

--- 2.ApplicationBusinessObjects/MedRec.Identity.BusinessObjects/Interfaces/Ports/IAuthenticateUserOutputPort.cs
+++ 2.ApplicationBusinessObjects/MedRec.Identity.BusinessObjects/Interfaces/Ports/IAuthenticateUserOutputPort.cs
@@ -2,7 +2,7 @@ using MedRec.BusinessObjects.Interfaces;
 using MedRec.Identity.BusinessObjects.DTOs;
 
 namespace MedRec.Identity.BusinessObjects.Interfaces.Ports;
-public interface IAuthenticateUserOutputPort : ICommonOutputPort
+public interface IAuthenticateUserOutputPort : IBaseOutputPort
 {
     Task Handle(AuthResultDto result, CancellationToken ct = default);
     Task InvalidCredentials();
